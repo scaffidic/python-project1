577ae2d9cff17930971cc3c641a7a47f66420909
scaffidic <cp.scaffidi@gmail.com>
2022-09-21 15:33:31 -0700

    added a while

--- project1.py
+++ project1.py
@@ -14,31 +14,33 @@ def main():
     print("\nReady for vacation? Hopefully I can help guide you to your dream destination!\n")
 
     # ask user for desired season and store in time_of_year variable
-    time_of_year = input(
+    season = input(
         "Please enter the season you would like to travel during:\n\tSpring, Summer, Fall, Winter\n> ")
 
     # lowercase user input
-    time_of_year = time_of_year.lower()
+    season = season.lower()
 
-    # print list of locations depending on user input for the season
-    if time_of_year == "spring":
-        print("Here is a list of some of the best vacations during the Spring!\n")
-        print(*spring_locations, sep=", ")
-
-    elif time_of_year == "summer":
-        print("Here is a list of some of the best vacations during the Summer!\n")
-        print(*summer_locations, sep=", ")
-
-    elif time_of_year == "fall":
-        print("Here is a list of some of the best vacations during the Fall!\n")
-        print(*fall_locations, sep=", ")
-
-    elif time_of_year == "winter":
-        print("Here is a list of some of the best vacations during the Winter!\n")
-        print(*winter_locations, sep=", ")
-
-    else:
-        print("Please enter one of the following:\n\tSpring, Summer, Fall, Winter")
+    while True:
+        # print list of locations depending on user input for the season
+        if season == "spring":
+            print("Here is a list of some of the best vacations during the Spring!\n")
+            print(*spring_locations, sep=", ")
+            break
+        elif season == "summer":
+            print("Here is a list of some of the best vacations during the Summer!\n")
+            print(*summer_locations, sep=", ")
+            break
+        elif season == "fall":
+            print("Here is a list of some of the best vacations during the Fall!\n")
+            print(*fall_locations, sep=", ")
+            break
+        elif season == "winter":
+            print("Here is a list of some of the best vacations during the Winter!\n")
+            print(*winter_locations, sep=", ")
+            break
+        else:
+            print("Please enter one of the following:\n\tSpring, Summer, Fall, Winter")
+            return
 
 
 if __name__ == "__main__":
